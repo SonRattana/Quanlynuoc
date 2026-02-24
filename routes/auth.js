dd805b02a0de273beb9fe3303f28f96f3de62229
SonRattana <sonrattan07@gmail.com>
2026-02-24 07:20:12 +0700

    login and register

--- routes/auth.js
+++ routes/auth.js
@@ -2,34 +2,65 @@ const router = require("express").Router();
 const db = require("../db");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
+const { body, validationResult } = require("express-validator");
 
 // REGISTER
-router.post("/register", async (req, res) => {
-  try {
-    const { username, email, password } = req.body;
+router.post(
+  "/register",
+  [
+    body("username").notEmpty().withMessage("Username is required"),
+    body("email").isEmail().normalizeEmail().withMessage("Invalid email"),
+    body("password")
+      .matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^A-Za-z\d]).{6,}$/)
+      .withMessage(
+        "Password must be at least 6 characters and include uppercase, lowercase, number and special character"
+      ),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
 
-    const hashedPassword = await bcrypt.hash(password, 10);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
 
-    await db.query(
-      "INSERT INTO users (username, email, password) VALUES (?, ?, ?)",
-      [username, email, hashedPassword]
-    );
+    try {
+      const { username, email, password } = req.body;
 
-    res.json({ message: "User registered" });
+      const hashedPassword = await bcrypt.hash(password, 10);
 
-  } catch (err) {
-    res.status(500).json(err);
+      await db.query(
+        "INSERT INTO users (username, email, password) VALUES (?, ?, ?)",
+        [username, email, hashedPassword]
+      );
+
+      res.json({ message: "User registered" });
+
+    } catch (err) {
+      console.log(err);
+      res.status(500).json({ error: "Server error" });
+    }
   }
-});
+);
 
 // LOGIN
-router.post("/login", async (req, res) => {
-  try {
-    const { email, password } = req.body;
+router.post("/login",
+  [
+    body("username").notEmpty().withMessage("Username is required"),
+    body("password").notEmpty().withMessage("Password is required"),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    try {
+    const { username, password } = req.body;
 
     const [rows] = await db.query(
-      "SELECT * FROM users WHERE email = ?",
-      [email]
+      "SELECT * FROM users WHERE username = ?",
+      [username]
     );
 
     if (rows.length === 0)
